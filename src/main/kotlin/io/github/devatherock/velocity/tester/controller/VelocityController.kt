c36675e20aaf5368edab013cb41be3c8778c92b4
devatherock <devatherock@gmail.com>
2021-03-11 22:03:23 -0600

    Closes #17, Closes #19
    ### Changed
    - Accepted requests with no
    `parameters`([#19](https://github.com/devatherock/velocity-template-tester/issues/19))
    
    ### Removed
    - Conversion to native
    image([#17](https://github.com/devatherock/velocity-template-tester/issues/17))

--- src/main/kotlin/io/github/devatherock/velocity/tester/controller/VelocityController.kt
+++ src/main/kotlin/io/github/devatherock/velocity/tester/controller/VelocityController.kt
@@ -1,7 +1,6 @@
 package io.github.devatherock.velocity.tester.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.github.devatherock.velocity.tester.entities.TemplateRequest
 import io.github.devatherock.velocity.tester.util.VelocityUtil
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
@@ -20,13 +19,13 @@ class VelocityController {
     fun expandJsonTemplate(@Body request: String): String {
         var parsedRequest = objectMapper.readValue(request, Map::class.java)
         return VelocityUtil.expandTemplate(parsedRequest.get("template") as String,
-                parsedRequest.get("parameters") as Map<String, Any>)
+                parsedRequest.get("parameters") as Map<String, Any>?)
     }
 
     @Post("/expandTemplate", produces = [MediaType.TEXT_PLAIN], consumes = [MediaType.APPLICATION_YAML])
     fun expandYamlTemplate(@Body request: String): String {
         var parsedRequest = Yaml().load<Map<String, Any>>(request)
         return VelocityUtil.expandTemplate(parsedRequest.get("template") as String,
-                parsedRequest.get("parameters") as Map<String, Any>)
+                parsedRequest.get("parameters") as Map<String, Any>?)
     }
 }
---
+++ src/test/groovy/io/github/devatherock/velocity/tester/controller/VelocityControllerSpec.groovy
@@ -0,0 +1,115 @@
+package io.github.devatherock.velocity.tester.controller
+
+import groovy.json.JsonOutput
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import org.yaml.snakeyaml.Yaml
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.inject.Inject
+
+/**
+ * Test class for {@link VelocityController}
+ */
+@MicronautTest
+class VelocityControllerSpec extends Specification {
+
+    @Inject
+    @Client('/')
+    HttpClient httpClient
+
+    @Shared
+    def requestWithParameters = [
+            'template'  : 'Hello ${user}',
+            'parameters': [
+                    'user': 'World!'
+            ]
+    ]
+
+    @Shared
+    def requestWithoutParameters = [
+            'template': 'Hello ${user}'
+    ]
+
+    @Unroll
+    void 'test expand template - no parameters, content type - #contentType'() {
+        when:
+        String response = httpClient.toBlocking().retrieve(
+                HttpRequest.POST('/api/expandTemplate', requestBody)
+                        .contentType(contentType))
+
+        then:
+        response == 'Hello ${user}'
+
+        where:
+        requestBody << [
+                JsonOutput.toJson(requestWithoutParameters),
+                new Yaml().dump(requestWithoutParameters)
+        ]
+        contentType << [
+                'application/json',
+                'application/x-yaml'
+        ]
+    }
+
+    @Unroll
+    void 'test expand template - parameters present, content type - #contentType'() {
+        when:
+        String response = httpClient.toBlocking().retrieve(
+                HttpRequest.POST('/api/expandTemplate', requestBody)
+                        .contentType(contentType))
+
+        then:
+        response == 'Hello World!'
+
+        where:
+        requestBody << [
+                JsonOutput.toJson(requestWithParameters),
+                new Yaml().dump(requestWithParameters)
+        ]
+        contentType << [
+                'application/json',
+                'application/x-yaml'
+        ]
+    }
+
+    @Unroll
+    void 'test expand template - conditional template, condition: #isMorning'() {
+        given:
+        def request = [
+                'template'  : '''
+                    #if(${morning})
+                      Good morning, ${user}
+                    #else
+                      Good evening, ${user}
+                    #end  
+                 ''',
+                'parameters': [
+                        'user'   : 'World!',
+                        'morning': isMorning
+                ]
+        ]
+
+        when:
+        String response = httpClient.toBlocking().retrieve(
+                HttpRequest.POST('/api/expandTemplate', JsonOutput.toJson(request))
+                        .contentType('application/json'))
+
+        then:
+        response == expectedOutput
+
+        where:
+        isMorning << [
+                'true',
+                'false'
+        ]
+        expectedOutput << [
+                'Good morning, World!',
+                'Good evening, World!'
+        ]
+    }
+}
