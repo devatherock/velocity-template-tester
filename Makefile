7a35661a7ea779764faf41c47277bf4470e67c31
devatherock <devatherock@gmail.com>
2024-09-20 19:23:37 -0500

    test: Added tests for the UI
    Closes #44

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 docker_tag=latest
 all=false
+ui_host=http://localhost:8080
+start_containers=true
 
 clean:
 	./gradlew clean
@@ -17,6 +19,16 @@ docker-run:
 	-p 8080:8080 \
 	devatherock/velocity-template-tester:$(docker_tag)
 integration-test:
-	DOCKER_TAG=$(docker_tag) docker-compose up &
+	DOCKER_TAG=$(docker_tag) docker compose up --wait
 	./gradlew integrationTest
-	docker-compose down	
+	docker-compose down
+ui-test:
+ifeq ($(start_containers), true)
+	DOCKER_TAG=$(docker_tag) docker compose up --wait
+endif	
+	UI_HOST=$(ui_host) npx playwright test
+ifeq ($(start_containers), true)
+	docker-compose down
+endif			
+ui-test-debug:
+	UI_HOST=$(ui_host) npx playwright test --ui --headed
---
+++ tests/index.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(process.env['UI_HOST'] || '');
+
+  await expect(page).toHaveTitle('Velocity Template Tester');
+});
+
+test.describe('Test template', () => {
+  test('Pass data model as properties', async ({ page }) => {
+    await page.locator('#template').fill('Hello ${user}!');
+    await page.locator('#dataModel').fill('user=World');
+    await page.locator('#eval-btn').click();
+
+    await expect(page.locator('#result')).toHaveValue('Hello World!');
+  });
+
+  test('Pass data model as JSON', async ({ page }) => {
+    await page.locator('#template').fill('Hello ${user}!');
+    await page.locator('#dataModel').fill(`{"user":"World"}`);
+    await page.locator('#eval-btn').click();
+
+    await expect(page.locator('#result')).toHaveValue('Hello World!');    
+  });
+});
